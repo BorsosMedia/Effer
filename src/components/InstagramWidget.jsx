03023547257900b0b0825b0c0b7d6b905bc4cf6c
zuritajonny <jonnyzuritadev@gmail.com>
2024-05-20 15:25:44 -0400

    Potential Script Improvements

--- src/components/InstagramWidget.jsx
+++ src/components/InstagramWidget.jsx
@@ -3,7 +3,11 @@ import { ElfsightWidget } from "react-elfsight-widget";
 function InstagramWidget() {
   return (
     <div className="IG-widget--wrapper">
-      <ElfsightWidget widgetID="1dc2a529-de8c-4f98-9613-f6a4084e674d" />
+      <ElfsightWidget
+        lazy
+        data-elfsight-app-lazy
+        widgetID="1dc2a529-de8c-4f98-9613-f6a4084e674d"
+      />
     </div>
   );
 }
--- src/components/TiktokWidget.jsx
+++ src/components/TiktokWidget.jsx
@@ -3,7 +3,11 @@ import { ElfsightWidget } from "react-elfsight-widget";
 function TiktokWidget() {
   return (
     <div className="Tiktok-widget--wrapper">
-      <ElfsightWidget widgetID="8a25e294-14c3-4743-a5bc-96c5705ba397" />
+      <ElfsightWidget
+        lazy
+        data-elfsight-app-lazy
+        widgetID="8a25e294-14c3-4743-a5bc-96c5705ba397"
+      />
     </div>
   );
 }
